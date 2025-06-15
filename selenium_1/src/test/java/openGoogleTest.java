524cc4a02a0533286a66f1b3b89cf62b4685fa8d
Kalani Nimnanjalee <137691801+nimnamjalee@users.noreply.github.com>
2025-06-15 13:01:05 +0530

    openGL

---
+++ selenium_1/src/test/java/openGoogleTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openGoogleTest {
+    @Test
+    public void googleTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+    }
+}
